babb8b1cb8ca27d0f77e28efe5cbcd66b2b94fdd
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-02-07 21:45:15 +0530

    Update main.py

--- Python/problem-set-2/1. Write a Python program to create a file where all letters of English alphabet are listed by specified number of letters on each line/main.py
+++ Python/problem-set-2/1. Write a Python program to create a file where all letters of English alphabet are listed by specified number of letters on each line/main.py
@@ -1,4 +1,6 @@
-file = open("alpha.txt","w")
-L = ["abcdefghijklmnopqrstuvwxyz"]
-file.writelines(L)
-file.close()
+no = int(input("Enter the number of letters per line: "))
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+with open("alphabet.txt", "w") as f:
+  
+ for i in range(0, len(alphabet), no):
+  f.write(alphabet.upper()[i:i+no] + "\n")
